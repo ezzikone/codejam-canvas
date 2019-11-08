1d6e28b2f08d009fdee67d7c52c56359c29a0339
ezzikone <47362010+ezzikone@users.noreply.github.com>
2019-11-09 02:18:02 +0400

    feat: deploy project

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import Canvas from "./js/Canvas.js";
-import Sizes from "./js/Size.js";
-import './scss/main.scss';
-
-const sizes = new Sizes(document.querySelector(".sizes"));
-const canvas = new Canvas(document.querySelector(".canvas"), sizes.getCurrentSize());
-sizes.onSizeChange(size => canvas.setSize(size));
